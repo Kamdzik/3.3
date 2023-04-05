8e613ebf0b9f3c46612df3b5080c596f53f1c06e
Kamdzik <127319303+Kamdzik@users.noreply.github.com>
2023-04-05 12:50:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+  public static void main(String[] args) {
+
+/*
+  narysuj 
+ "*****
+   ****
+     **
+      *
+       
+
+  
+*/
+    for(int a = 4 ; a>=0;a--){
+      System.out.println("");
+      
+       for(int c = 4 ; c>a; c--){
+         System.out.print(" ");
+      } 
+      
+      for(int b = 0 ; b<=a; b++){   
+        System.out.print("*");
+      }
+    }
+
+
+    
+  }
+}
